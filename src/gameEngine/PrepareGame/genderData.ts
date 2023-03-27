d2d0e3035117c5b7ad82a854c53a21778fa2eff1
Nikita Kolotenko <tofeee63@gmail.com>
2023-03-27 13:28:28 +0300

    starting write gender generator

---
+++ src/gameEngine/PrepareGame/genderData.ts
@@ -0,0 +1,9 @@
+import randomizer from '../../helpers/randomizer';
+
+const genderData = (playersCount: number) => {
+	const data = [];
+
+	do {} while (data.length === playersCount);
+};
+
+export default genderData;
